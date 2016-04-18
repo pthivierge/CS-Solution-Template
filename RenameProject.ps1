60f0a36976143ab85140ae06dd7fe51267e6b411
pthivierge <pthivierge@osisoft.com>
2016-04-18 16:30:25 +0200

    fix for issue #1 - added a rename for the short service name

--- RenameProject.ps1
+++ RenameProject.ps1
@@ -3,21 +3,23 @@
 # Current values are examples.
 # You must change ALL of them
 # Company Official Name
-$ASM_COMPANY="Company"
+$ASM_COMPANY="ACME"
 
 #Long product name - give a good description here
-$ASM_PRODUCT="Process Data Calculation Service"
+$ASM_PRODUCT="ACME super product"
 
-$ASM_SERVICE_DISPLAY_NAME="Process Data Calculation Service"
-
-$ASM_SERVICE_DESCRIPTION="Service that calculates data for equipment XYZ"
+# Service name (short name)
+$ASM_SERVICENAME="ACMEservice"
+# Service display name
+$ASM_SERVICE_DISPLAY_NAME="ACME Super Service"
+$ASM_SERVICE_DESCRIPTION="ACME Service that computes XYZ"
 
 # Current year
 $YEAR="2015"
 
 # New short name of the application (Short one word name) - used in Namespace and files naming 
 # In one word, no space, no special character
-$SHORT_NAME="PDR"
+$SHORT_NAME="ACME"
 
 #
 # END SETTINGS
@@ -33,7 +35,7 @@ foreach ($file in $configFiles)
 {
     Write-Host $file.Name
     (Get-Content $file.PSPath) | 
-    Foreach-Object {$_ -replace "NewApp", $SHORT_NAME -replace "%ASM_COMPANY%", $ASM_COMPANY -replace "%ASM_PRODUCT%", $ASM_PRODUCT -replace "%YEAR%", $YEAR -replace "%ASM_SERVICE_DISPLAY_NAME%",$ASM_SERVICE_DISPLAY_NAME -replace "%ASM_SERVICE_DESC%",$ASM_SERVICE_DESCRIPTION  } | 
+    Foreach-Object {$_ -replace "NewApp", $SHORT_NAME -replace "%ASM_COMPANY%", $ASM_COMPANY -replace "%ASM_PRODUCT%", $ASM_PRODUCT -replace "%YEAR%", $YEAR -replace "%ASM_SERVICE_DISPLAY_NAME%",$ASM_SERVICE_DISPLAY_NAME -replace "%ASM_SERVICE_DESC%",$ASM_SERVICE_DESCRIPTION -replace "NewAppService",$ASM_SERVICENAME  } | 
     Set-Content $file.PSPath
     Rename-Item -Path $file.FullName -NewName ($file.Name -replace "NewApp",$SHORT_NAME)
 }
