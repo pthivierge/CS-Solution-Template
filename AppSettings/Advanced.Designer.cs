9664b4647c7a64aecca02f551f3838ebff008a6d
Pat <pthivierge@ptf-consulting.com>
2021-02-06 16:34:17 +0100

    Updated packages and .net version to 4.6.1

--- AppSettings/Advanced.Designer.cs
+++ AppSettings/Advanced.Designer.cs
@@ -12,7 +12,7 @@ namespace NewApp.Settings {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     public sealed partial class Advanced : global::System.Configuration.ApplicationSettingsBase {
         
         private static Advanced defaultInstance = ((Advanced)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Advanced())));
--- AppSettings/General.Designer.cs
+++ AppSettings/General.Designer.cs
@@ -12,7 +12,7 @@ namespace NewApp.Settings {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     public sealed partial class General : global::System.Configuration.ApplicationSettingsBase {
         
         private static General defaultInstance = ((General)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new General())));
